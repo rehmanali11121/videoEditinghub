3955fc0701b05eb8749715a7fe08d2b5f524686f
Adil-Abid <111174935+Adil-Abid@users.noreply.github.com>
2023-07-31 20:45:39 +0500

    public Folder files

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -52,7 +52,7 @@
             </div>
 		</div>
 
-    <!-- <script src="{{ asset('js/jquery.min.js') }}"></script> -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script> 
     <script src="{{ asset('js/popper.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,3 +1,294 @@
 <h1>User Dashboard</h1>
 
 <a href="/logout">Logout</a>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>V.E.H</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="./css/CenturyGothic.ttf" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+  <style>
+    /* .star-wrapper {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    direction: rtl;
+  } */
+    .star-wrapper a {
+      font-size: 4em;
+      color: #fff;
+      text-decoration: none;
+      transition: all 0.5s;
+      margin: 4px;
+    }
+  
+    .star-wrapper a:hover {
+      color: gold;
+      transform: scale(1.3);
+    }
+  
+    .s1:hover~a {
+      color: gold;
+    }
+  
+    .s2:hover~a {
+      color: gold;
+    }
+  
+    .s3:hover~a {
+      color: gold;
+    }
+  
+    .s4:hover~a {
+      color: gold;
+    }
+  
+    .s5:hover~a {
+      color: gold;
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="hero_area">
+  <!-- header section strats -->
+  <header class="header_section">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg custom_nav-container">
+        <a class="navbar-brand" href="index.html">
+          <span>
+            <img src="./images/video_editing_logo.png" width="200px" alt="VideoEditingHub">
+          </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="s-1"> </span>
+          <span class="s-2"> </span>
+          <span class="s-3"> </span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html"> About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="shop.html"> Shop</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="report_us.html"> Report us </a>
+              </li>
+              <li class="nav-item">
+                <div class="btn-box pt-2 row justify-content-center">
+                  <a href="signup.html" class="btn-1 col-md-5 col-5 mr-1">Sign Up</a>
+                  <a href="login.html" class="btn-2 col-md-5 col-5 mr-1">Login</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section ">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="detail_box">
+                    <h1 class="pb-3">Welcome to The V.E.H</h1>
+                    <p class="pb-3">We bring your vision to life through professional video editing. Our skilled team is dedicated to
+                      delivering exceptional results that exceed your expectations.</p>
+                      <a href="shop.html" class="btn-1">Shop</a>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="images/slider-img.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6" >
+                  <div class="detail_box">
+                    <h1 class="pb-3">The Best Video Editing Hub</h1>
+                    <p class="pb-3">We're here to help .contact us today and let us turn your raw footage into a captivating visual masterpiece.<br>Let's make your video shine!</p>
+                 
+                      <a href="shop.html" class="btn-1">Shop</a>
+                    
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="images/slider-img.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel_btn-container">
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+
+  <!-- about section -->
+
+    <section class="about_section pt-0 pb-0">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="img-box">
+              <img src="images/about-img2.png" alt="">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="detail-box">
+              <div class="heading_container">
+                <h2>
+                  About Us
+                </h2>
+              </div>
+              <p class="font-large">
+                We bring your vision to life through professional video editing. Our skilled team is dedicated to delivering exceptional
+                results that exceed your expectations.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End about section -->
+  <!-- Hiring Section -->
+  <section class="hiring_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/hiring.jpg" alt="" width="100%">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="detail-box">
+            <div class="heading_container pb-4">
+              <h2>
+                We are Willing to hire
+              </h2>
+            </div>
+            <p class="font-large">
+              V.E.H (Video Editing Hub) is expanding and seeking 10 new editors each month to meet the increasing demand for
+              exceptional video editing services. We prioritize both speed and quality, ensuring timely deliveries. Join our team to
+              showcase your editing skills, grow professionally, and collaborate in a supportive environment. Let's create
+              extraordinary videos together and empower talented editors. To apply, please email videoediting.pro@gmail.com with three
+              of your recent editing projects, along with your resume and a brief statement explaining why you want to be part of the
+              Video Editing Hub team. We look forward to reviewing your work and learning more about you
+            </p>
+          </div>
+        </div>
+        
+        
+      </div>
+    </div>
+  </section>
+  <!--End Hiring Section -->
+  <div class="footer_bg">
+  
+    <!-- footer section -->
+    <section class="container-fluid footer_section">
+      <div class="container">
+        <div class="row justify-content-end">
+          <div class="col-md-5">
+            <h4 class="text-white text-end text-right">Add Review</h4>
+            <form action="">
+              <div class="row">
+                <div class="col-md-4 col-5 mt-3">
+                  <a href="review.html" class="text-link">View Reviews</a>
+                </div>
+                <div class="col-md-8 col-7 ">
+                  <div class="rate">
+                    <input type="radio" id="star5" name="rate" value="5" />
+                    <label for="star5" title="text">5 stars</label>
+                    <input type="radio" id="star4" name="rate" value="4" />
+                    <label for="star4" title="text">4 stars</label>
+                    <input type="radio" id="star3" name="rate" value="3" />
+                    <label for="star3" title="text">3 stars</label>
+                    <input type="radio" id="star2" name="rate" value="2" />
+                    <label for="star2" title="text">2 stars</label>
+                    <input type="radio" id="star1" name="rate" value="1" />
+                    <label for="star1" title="text">1 star</label>
+                  </div>
+                </div>
+              </div>
+              <textarea placeholder="Write your review" class="form-control"></textarea>
+              <div class="row justify-content-center">
+  
+                <a type="submit" class="btn-2 w-100 m-3 " style="border-radius: 5px;">Submit</a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <p class="font-large">
+              &copy; <span id="displayYear"></span> All Rights Reserved By
+              <a href="/">VideoEditingHub.Pro</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end footer_section -->
+  </div>
+
+  <script src="https://kit.fontawesome.com/5ea815c1d0.js"></script>
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+
+</html>
